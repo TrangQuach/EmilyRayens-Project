8954066606dbd8da5549ed90b89ad56603b56ea1
Emily Rayens <ear42390@uga.edu>
2019-10-11 15:45:24 -0400

    Update

--- code/analysis_code/analysisscript.R
+++ code/analysis_code/analysisscript.R
@@ -4,23 +4,27 @@
 #this script loads the processed, cleaned data, does a simple analysis
 #and saves the results to the results folder
 
+library(readxl)
+Pc <- read_excel("data/raw_data/Pc.xlsx")
 
 library(ggplot2)
 library(dplyr)
 library(broom)
+library(kableExtra)
 
 
-p1 <- Pc %>% ggplot(aes(x=Neutrophils, y=PcStatus)) + geom_point() + geom_smooth(method='lm')
+p1 <- Pc %>% ggplot(aes(x=PcStatus, y=Neutrophils)) + geom_point() + geom_smooth(method='lm')
 
+p2 <- Pc %>% ggplot(aes(x=Vaccinated, y=Neutrophils)) + geom_bar(stat="identity") 
 
-#save figure
+#save figures
 
 ggsave(filename="./results/resultfigure.png",plot=p1) 
-
+ggsave(filename="./results/resultfigure2.png",plot=p2) 
 
 # fit linear model
 
-lmfit <- lm(Neutrophils ~ PcStatus, Pc)  
+lmfit <- lm(PcStatus ~ Neutrophils, Pc)  
 
 # place results from fit into a data frame with the tidy function
 
@@ -31,4 +35,13 @@ lmtable <- broom::tidy(lmfit)
 saveRDS(lmtable, file = "./results/resulttable.rds")
 
 
+# make a table
+a1 <- aggregate(Pc[, 3:9], list(Pc$Identifier), mean, na.rm=TRUE)
+a2 <- aggregate(Pc[, 3:9], list(Pc$Vaccinated), mean, na.rm=TRUE)
+
+t1 <- kable(a1, caption = "Table 1")
+t2 <- kable(a2, caption = "Table 2")
+
+saveRDS(t1, file = "./results/resulttable.rds")
+saveRDS(t2, file = "./results/resulttable2.rds")
   
